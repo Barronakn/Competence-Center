5dbfdbd4a1bb289e2b97b37ea43f356a13faf7dc
Barron <barronakn@gmail.com>
2023-04-09 22:24:07 +0100

    Remove node

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,8 +23,7 @@ const componentName = (props) => {
         >
           <Navigation />
         </motion.div>
-        <Item1 />
-        <div data-aos="fade-up">
+        <Item1 />        <div data-aos="fade-up">
           <Item2 />
         </div>
         <Item3 />
